f2a1edb835a9f0f78aeed42558becc3004cab6c6
Keiland Fonteno <89884205+Kfont400@users.noreply.github.com>
2022-05-24 10:36:22 -0500

    Exception Handling
    Added exception handling to improve the usability.

--- main.py
+++ main.py
@@ -4,12 +4,17 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_password():
+  word = input("Enter a word: ").upper()
+  try:
+    output_list = [phonetic_dict[letter] for letter in word]
+  except KeyError:
+    print("Please use words that contain letters only")
+    generate_password()
+  else:
+    print(output_list)
+  
+generate_password()
